3a66dea4914e1aa1a37d0fd7e1919690ab4f1623
Sahil Makhijani <sahil.makhijani@gmail.com>
2021-05-11 17:44:50 +0530

    Add isWhite and isBlack methods to Player

--- src/vp/chess/core/PlayerEnum.java
+++ src/vp/chess/core/PlayerEnum.java
@@ -8,4 +8,12 @@ public enum PlayerEnum {
     public Color getColor() {
         return this.name() == PlayerEnum.WHITE.name() ? Color.WHITE : Color.BLACK;
     }
+
+    public boolean isWhite() {
+        return (this.equals(PlayerEnum.WHITE));
+    }
+
+    public boolean isBlack() {
+        return (this.equals(PlayerEnum.BLACK));
+    }
 }
